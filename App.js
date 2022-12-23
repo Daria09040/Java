e3cd691af0255aa65a7940cf0b81d1ba14d17a3a
Panfilova Daria <93768843+Daria09040@users.noreply.github.com>
2022-12-23 23:50:51 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в планировщик заданий!");
+        TaskMgr taskMgr = new TaskMgr();
+
+    }
+}
